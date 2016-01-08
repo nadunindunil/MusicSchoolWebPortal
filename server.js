441be0c5e83b4353e59902e638bcb4170d96671a
nadunindunil <nadun1indunil@gmail.com>
2016-01-08 12:55:43 +0530

    requsts were added to route.js

--- server.js
+++ server.js
@@ -18,15 +18,7 @@ var connection = mysql.createConnection({
 	  database : 'g19'
 });
 
-app.get('/api/getData', function(req, res) {
-            
 
-           	  connection.query('SELECT * FROM student', function(err, rows, fields) {
-			    if (err) throw err;
-			    res.json(rows);
-			    console.log('The solution is: ', rows);
-			  });
-    });
 
 
 
@@ -43,9 +35,10 @@ app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-M
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
 // routes ==================================================
-require('./app/routes')(app); // pass our application into our routes
+require('./app/routes')(app,connection); // pass our application into our routes
 
 // start app ===============================================
 app.listen(port);	
 console.log('Magic happens on port ' + port); 			// shoutout to the user
 exports = module.exports = app; 						// expose app
+module.exports = connection;
