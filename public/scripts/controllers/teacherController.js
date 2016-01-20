5adcadcf91c516940ff4a00cccd04e5945ce7947
nadunindunil <nadun1indunil@gmail.com>
2016-01-20 19:11:51 +0530

    working on adding attendance

--- public/scripts/controllers/teacherController.js
+++ public/scripts/controllers/teacherController.js
@@ -5,7 +5,32 @@
 angular.module('sbAdminApp')
     .controller('TeachersCtrl',['$scope','$rootScope','$http','$log', function($scope,$rootScope,$http,$log){
 
+        var d = new Date();
+        var month = new Array();
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
 
+        $scope.thisMonth = month[d.getMonth()];  //this month
+
+        $scope.thisYear = d.getFullYear(); // this year
+
+        $scope.thisDate = d.getDate();  // current date
+
+
+    $scope.settID = function(id){
+
+        $scope.thisTID = id;
+    };
 
     $scope.teachInit = function(){
         //$rootScope.pageName = "TEACHERS MANAGEMENT";
@@ -30,6 +55,8 @@ angular.module('sbAdminApp')
             });
 
     };
+
+
 
     $scope.addTeacher = function(){
 
